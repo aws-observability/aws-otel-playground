98183bc9f396e4638498eacf8de97b8650716585
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-07-07 17:13:37 +0900

    Update agent

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -38,7 +38,7 @@ val DEPENDENCY_SETS = listOf(
         ),
         DependencySet(
                 "io.opentelemetry.instrumentation.auto",
-                "0.5.0-20200701.032958-14",
+                "0.5.0-20200706.134853-20",
                 listOf(
                         "opentelemetry-javaagent",
                         "opentelemetry-auto-exporter-otlp"
